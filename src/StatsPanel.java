37007bfc29b010684966ef77673cce95c5349251
dswhaley <147189668+dswhaley@users.noreply.github.com>
2026-02-10 23:52:20 -0500

    Refactored Stats Panel

--- src/StatsPanel.java
+++ src/StatsPanel.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+
 /**
  * Displays statistics about how many guesses the person took during past games
  * Loads data from the file and displays in a JPanel
@@ -10,26 +11,34 @@ import java.util.ArrayList;
  */
 public class StatsPanel extends JPanel {
 
+
     private final JPanel resultsPanel;
+
 
     // Stats will display the number of games in each "bin"
     // A bin goes from BIN_EDGES[i] through BIN_EDGES[i+1]-1, inclusive
     private static final int [] BIN_EDGES = {1, 2, 4, 6, 8, 10, 12, 14};
     private ArrayList<JLabel> resultsLabels;
 
+
     public StatsPanel(JPanel cardsPanel) {
 
+
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
 
         JLabel title = new JLabel("Your Stats");
         this.add(title);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+
         JLabel subtitle = new JLabel("(Past 30 Days)");
         this.add(subtitle);
         subtitle.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+
         this.add(Box.createRigidArea(new Dimension(0,40)));
+
 
         resultsPanel = new JPanel();
         resultsLabels = new ArrayList<>();
@@ -52,18 +61,22 @@ public class StatsPanel extends JPanel {
                 }
             }
 
+
             resultsPanel.add(new JLabel(binName));
             JLabel result = new JLabel("--");
             resultsLabels.add(result);
             resultsPanel.add(result);
         }
 
+
         resultsPanel.setMinimumSize(new Dimension(120, 120));
         this.add(resultsPanel);
         resultsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         updateResultsPanel();
 
+
         this.add(Box.createVerticalGlue());
+
 
         JButton quit = new JButton("Back to Home");
         quit.addActionListener(e -> {
@@ -74,7 +87,9 @@ public class StatsPanel extends JPanel {
         this.add(quit);
         quit.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+
         this.add(Box.createRigidArea(new Dimension(0,20)));
+
 
         this.addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentShown(java.awt.event.ComponentEvent e) {
@@ -84,20 +99,26 @@ public class StatsPanel extends JPanel {
     }
 
 
+
+
     private void clearResults(){
         for(JLabel lbl : resultsLabels){
             lbl.setText("--");
         }
     }
 
+
     private void updateResultsPanel(){
         clearResults();
 
+
         GameStats stats = new StatsFile();
+
 
         for(int binIndex=0; binIndex<BIN_EDGES.length; binIndex++){
             final int lowerBound = BIN_EDGES[binIndex];
             int numGames = 0;
+
 
             if(binIndex == BIN_EDGES.length-1){
                 // last bin
@@ -113,8 +134,10 @@ public class StatsPanel extends JPanel {
                 }
             }
 
+
             JLabel resultLabel = resultsLabels.get(binIndex);
             resultLabel.setText(Integer.toString(numGames));
         }
     }
 }
+
